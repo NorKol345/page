df92e8e26be3a8341ea5fe9a0e1b520e654a46b7
Norbert Jakub <nkolodziej@poczta.onet.pl>
2022-05-14 22:04:26 +0200

    js added

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// DOM
+const hamburger = document.querySelector('.top span');
+
+const nav = document.querySelector('nav');
+
+const styleNav = getComputedStyle(nav);
+
+hamburger.addEventListener('click', () => {
+
+    if(styleNav.display == 'none') 
+    {
+        nav.style.display = 'block';
+
+        hamburger.innerText = 'close'
+    } else
+    {
+        hamburger.innerText = 'menu'
+        nav.style.display = 'none';
+
+    }
+        
+});
+
